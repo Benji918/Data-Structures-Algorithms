2fadad0b93d583cc365789bda6aa0e45866482a7
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-05-17 22:36:19 +0100

    initial commit

---
+++ Binary search.py
@@ -0,0 +1,5 @@
+def binary_search(list, target):
+    """
+    Returns the index position of the target if found, else returns None
+    """
+    pass
---
+++ Linear search.py
@@ -0,0 +1,13 @@
+def linear_search(list, target):
+    """
+    Returns the index position of the target if found, else returns None
+    """
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+
+
+print(linear_search([1,2,3,4,5], 15))
+
